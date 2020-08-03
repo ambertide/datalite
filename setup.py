a6f5be4f0f4abfca176e40c1fdae1613b7a2690f
Ege Emir Özkan <egeemirozkan24@gmail.com>
2020-08-03 03:33:01 +0300

    Add some setup related stuff.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datalite", # Replace with your own username
+    version="0.1",
+    author="Ege Ozkan",
+    author_email="egeemirozkan24@gmail.com",
+    description="A small package that binds dataclasses to an sqlite database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ambertide/datalite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
